df2ec9afbf11ef5c962b50bfe69e2500141f7fbd
Kartik Choudhary <kartikchoudhary9@gmail.com>
2023-11-01 18:40:42 +0000

    removed unnecessary dependencies

--- src/llm_wrappers/__init__.py
+++ src/llm_wrappers/__init__.py
@@ -1,2 +1,11 @@
-from llm_wrappers.wrappers.openai_wrapper import OpenAIWrapper
-from llm_wrappers.wrappers.llama_wrapper import LlamaWrapper
+import logging
+
+try:
+    from llm_wrappers.wrappers.openai_wrapper import OpenAIWrapper
+except ImportError:
+    logging.warning('OpenAIWrapper not imported. OpenAI API not available.')
+
+try:
+    from llm_wrappers.wrappers.llama_wrapper import LlamaWrapper
+except ImportError:
+    logging.warning('LlamaWrapper not imported. Llama API not available.')
